2366aea31a1911e9a435bf47846a4f95e7189c9f
Mikhail Murtazaliev <volboy@yandex.ru>
2020-11-17 14:13:39 +0400

    Add fragments and make navigation

--- app/src/main/java/com/volboy/learnworlds/LearnWords.kt
+++ app/src/main/java/com/volboy/learnworlds/LearnWords.kt
@@ -5,55 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.volboy.learnworlds.databinding.FragmentLearnWordsBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [LearnWords.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LearnWords : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentLearnWordsBinding
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate<FragmentLearnWordsBinding>(inflater, R.layout.fragment_learn_words, container, false)
+        setListeners()
+        return binding.root
+    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private fun setListeners() {
+        val clickableViews: List<View> = listOf(binding.itemENGRUS, binding.itemRUSENG, binding.itemTRANSCRIP, binding.itemRANDOM)
+        for (item in clickableViews) {
+            item.setOnClickListener { choiceLearnMode(it) }
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_learn_words, container, false)
-    }
+    private fun choiceLearnMode(view: View) {
+        when (view) {
+            binding.itemENGRUS -> Toast.makeText(context, "Translate from English to Russian", Toast.LENGTH_SHORT).show()
+            binding.itemRUSENG -> Toast.makeText(context, "Translate from Russian to English", Toast.LENGTH_SHORT).show()
+            binding.itemTRANSCRIP -> Toast.makeText(context, "Translate from Transcription to word", Toast.LENGTH_SHORT).show()
+            binding.itemRANDOM -> Toast.makeText(context, "Random mode", Toast.LENGTH_SHORT).show()
+            else -> Toast.makeText(context, "Choice the mode", Toast.LENGTH_SHORT).show()
+        }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LearnWords.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LearnWords().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
     }
 }
--- app/src/main/java/com/volboy/learnworlds/MainActivity.kt
+++ app/src/main/java/com/volboy/learnworlds/MainActivity.kt
@@ -2,10 +2,16 @@ package com.volboy.learnworlds
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.volboy.learnworlds.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding= DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
+        //val navigationController=this.findNavController(R.navigation.navigation)
+
     }
 }
