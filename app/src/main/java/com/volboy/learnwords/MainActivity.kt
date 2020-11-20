a253809896189b017389bc4f9f7f241381764c21
Mikhail Murtazaliev <volboy@yandex.ru>
2020-11-20 15:02:43 +0400

    Add drawerLayout

--- app/src/main/java/com/volboy/learnwords/MainActivity.kt
+++ app/src/main/java/com/volboy/learnwords/MainActivity.kt
@@ -3,22 +3,40 @@ package com.volboy.learnwords
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.volboy.learnwords.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding =
             DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        drawerLayout = binding.drawerLayout
         val navigationController = this.findNavController(R.id.navigationHostFragment)
-        NavigationUI.setupActionBarWithNavController(this, navigationController)
+        NavigationUI.setupActionBarWithNavController(this, navigationController, drawerLayout)
+        appBarConfiguration= AppBarConfiguration(navigationController.graph, drawerLayout)
+        navigationController.addOnDestinationChangedListener { nc: NavController, nd: NavDestination, bundle: Bundle? ->
+            if (nd.id == nc.graph.startDestination) {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            } else {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            }
+        }
+        NavigationUI.setupWithNavController(binding.navigationView, navigationController)
+
 
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navigationController = this.findNavController(R.id.navigationHostFragment)
-        return navigationController.navigateUp()
+        return NavigationUI.navigateUp(navigationController, appBarConfiguration)
     }
 }
